7be26d4e8235007ed017c49174dbf3c9e2dee728
tbwebb22 <tbwebb22@gmail.com>
2023-06-26 14:59:20 -0400

    Add tests

--- test/LockRelease.test.ts
+++ test/LockRelease.test.ts
@@ -23,39 +23,45 @@ describe("LockRelease", async function () {
   let startTime: number;
   let duration: number;
 
-  beforeEach(async function () {
-    [deployer, owner, beneficiary1, beneficiary2, beneficiary3, beneficiary4] =
-      await ethers.getSigners();
-
-    // Deploy contracts
-    dcnt = await new DCNTToken__factory(deployer).deploy(1000, owner.address);
-
-    // Token unlocks start in 100 seconds from current time, and vesting lasts for 100 seconds
-    startTime = (await time.latest()) + 100;
-    duration = 100;
-
-    lockRelease = await new LockRelease__factory(deployer).deploy(
-      dcnt.address,
-      [
-        beneficiary1.address,
-        beneficiary2.address,
-        beneficiary3.address,
-        beneficiary4.address,
-      ],
-      [100, 200, 300, 400],
-      startTime,
-      duration
-    );
-
-    await dcnt.connect(deployer).transfer(lockRelease.address, 1000);
-
-    await dcnt.connect(beneficiary1).delegate(beneficiary1.address);
-    await dcnt.connect(beneficiary2).delegate(beneficiary2.address);
-    await dcnt.connect(beneficiary3).delegate(beneficiary3.address);
-    await dcnt.connect(beneficiary4).delegate(beneficiary4.address);
-  });
-
   describe("LockRelease Features", function () {
+    beforeEach(async function () {
+      [
+        deployer,
+        owner,
+        beneficiary1,
+        beneficiary2,
+        beneficiary3,
+        beneficiary4,
+      ] = await ethers.getSigners();
+
+      // Deploy contracts
+      dcnt = await new DCNTToken__factory(deployer).deploy(1000, owner.address);
+
+      // Token unlocks start in 100 seconds from current time, and vesting lasts for 100 seconds
+      startTime = (await time.latest()) + 100;
+      duration = 100;
+
+      lockRelease = await new LockRelease__factory(deployer).deploy(
+        dcnt.address,
+        [
+          beneficiary1.address,
+          beneficiary2.address,
+          beneficiary3.address,
+          beneficiary4.address,
+        ],
+        [100, 200, 300, 400],
+        startTime,
+        duration
+      );
+
+      await dcnt.connect(deployer).transfer(lockRelease.address, 1000);
+
+      await dcnt.connect(beneficiary1).delegate(beneficiary1.address);
+      await dcnt.connect(beneficiary2).delegate(beneficiary2.address);
+      await dcnt.connect(beneficiary3).delegate(beneficiary3.address);
+      await dcnt.connect(beneficiary4).delegate(beneficiary4.address);
+    });
+
     it("Is initialized correctly", async function () {
       expect(await dcnt.balanceOf(lockRelease.address)).to.eq(1000);
       expect(await dcnt.balanceOf(deployer.address)).to.eq(0);
@@ -435,6 +441,153 @@ describe("LockRelease", async function () {
       expect(await lockRelease.getVotes(beneficiary2.address)).to.eq(200);
       expect(await lockRelease.getVotes(beneficiary3.address)).to.eq(300);
       expect(await lockRelease.getVotes(beneficiary4.address)).to.eq(400);
+    });
+
+    it("LockRelease correctly tracks user's past votes", async function () {
+      // Increase time to 10% through vesting period
+      await time.increaseTo(startTime + 10);
+
+      const blockOneNumber = (await ethers.provider.getBlock("latest")).number;
+      const blockOneVotes = 100;
+      expect(await lockRelease.getVotes(beneficiary1.address)).to.eq(
+        blockOneVotes
+      );
+
+      await time.increaseTo(startTime + 49);
+      await lockRelease.connect(beneficiary1).release();
+      // Should have 50 votes directly on token, and 50 on LockRelease
+
+      // beneficiary 1 delegates their lockRelease votes to beneficiary 2
+      await lockRelease.connect(beneficiary1).delegate(beneficiary2.address);
+      const blockTwoNumber = (await ethers.provider.getBlock("latest")).number;
+      const blockTwoVotes = 50;
+      expect(await lockRelease.getVotes(beneficiary1.address)).to.eq(
+        blockTwoVotes
+      );
+
+      // beneficiary 1 delegates their dcnt votes to beneficiary 2
+      await dcnt.connect(beneficiary1).delegate(beneficiary2.address);
+      const blockThreeNumber = (await ethers.provider.getBlock("latest"))
+        .number;
+      const blockThreeVotes = 0;
+      expect(await lockRelease.getVotes(beneficiary1.address)).to.eq(
+        blockThreeVotes
+      );
+
+      await time.increase(1);
+
+      // Check votes at past blocks are correct
+      expect(
+        await lockRelease.getPastVotes(beneficiary1.address, blockOneNumber)
+      ).to.eq(blockOneVotes);
+      expect(
+        await lockRelease.getPastVotes(beneficiary1.address, blockTwoNumber)
+      ).to.eq(blockTwoVotes);
+      expect(
+        await lockRelease.getPastVotes(beneficiary1.address, blockThreeNumber)
+      ).to.eq(blockThreeVotes);
+
+      // benefeciary 1 re-delegates back to themselves
+      await lockRelease.connect(beneficiary1).delegate(beneficiary1.address);
+      await dcnt.connect(beneficiary1).delegate(beneficiary1.address);
+      const blockFourNumber = (await ethers.provider.getBlock("latest")).number;
+      const blockFourVotes = 100;
+      expect(await lockRelease.getVotes(beneficiary1.address)).to.eq(
+        blockFourVotes
+      );
+
+      // advance time to after end of vesting period
+      await time.increaseTo(startTime + duration + 10);
+
+      // beneficiary 1 releases the remaining of their locked tokens
+      await lockRelease.connect(beneficiary1).release();
+
+      const blockFiveNumber = (await ethers.provider.getBlock("latest")).number;
+      const blockFiveVotes = 100;
+      expect(await lockRelease.getVotes(beneficiary1.address)).to.eq(
+        blockFiveVotes
+      );
+
+      await dcnt.connect(beneficiary1).transfer(beneficiary2.address, 30);
+      const blockSixNumber = (await ethers.provider.getBlock("latest")).number;
+      const blockSixVotes = 70;
+      expect(await lockRelease.getVotes(beneficiary1.address)).to.eq(
+        blockSixVotes
+      );
+
+      await dcnt.connect(beneficiary1).transfer(beneficiary2.address, 60);
+      const blockSevenNumber = (await ethers.provider.getBlock("latest"))
+        .number;
+      const blockSevenVotes = 10;
+      expect(await lockRelease.getVotes(beneficiary1.address)).to.eq(
+        blockSevenVotes
+      );
+
+      await time.increase(1);
+
+      // Check votes at past blocks are correct
+      expect(
+        await lockRelease.getPastVotes(beneficiary1.address, blockOneNumber)
+      ).to.eq(blockOneVotes);
+      expect(
+        await lockRelease.getPastVotes(beneficiary1.address, blockTwoNumber)
+      ).to.eq(blockTwoVotes);
+      expect(
+        await lockRelease.getPastVotes(beneficiary1.address, blockThreeNumber)
+      ).to.eq(blockThreeVotes);
+      expect(
+        await lockRelease.getPastVotes(beneficiary1.address, blockFourNumber)
+      ).to.eq(blockFourVotes);
+      expect(
+        await lockRelease.getPastVotes(beneficiary1.address, blockFiveNumber)
+      ).to.eq(blockFiveVotes);
+      expect(
+        await lockRelease.getPastVotes(beneficiary1.address, blockSixNumber)
+      ).to.eq(blockSixVotes);
+      expect(
+        await lockRelease.getPastVotes(beneficiary1.address, blockSevenNumber)
+      ).to.eq(blockSevenVotes);
+    });
+
+    it("Reverts when getPastVotes is called on the current or a future block", async function () {
+      await expect(
+        lockRelease.getPastVotes(
+          beneficiary1.address,
+          (
+            await ethers.provider.getBlock("latest")
+          ).number
+        )
+      ).to.be.revertedWith("ERC20Votes: future lookup");
+
+      await expect(
+        lockRelease.getPastVotes(
+          beneficiary1.address,
+          (await ethers.provider.getBlock("latest")).number + 1
+        )
+      ).to.be.revertedWith("ERC20Votes: future lookup");
+    });
+
+    it.only("Reverts when release is called and releasable amount is zero", async function () {
+      // vesting hasn't started, so releasable amount is zero
+      expect(await lockRelease.getReleasable(beneficiary1.address)).to.eq(0);
+      await expect(
+        lockRelease.connect(beneficiary1).release()
+      ).to.be.revertedWith("NothingToRelease()");
+
+      // deployer isn't a beneficiary, so shouldn't have any releasable funds
+      expect(await lockRelease.getReleasable(deployer.address)).to.eq(0);
+      await expect(lockRelease.connect(deployer).release()).to.be.revertedWith(
+        "NothingToRelease()"
+      );
+
+      // Increase time to after vesting period
+      await time.increaseTo(startTime + duration + 10);
+
+      // deployer isn't a beneficiary, so shouldn't have any releasable funds
+      expect(await lockRelease.getReleasable(deployer.address)).to.eq(0);
+      await expect(lockRelease.connect(deployer).release()).to.be.revertedWith(
+        "NothingToRelease()"
+      );
     });
   });
 });
