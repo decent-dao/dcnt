14c57ac144689980c925a8ca8e64094b29c93a20
sethhrbek <seth.hrbek@gmail.com>
2023-07-11 14:55:17 -0400

    add scripts + refactor DCNT token tests to use context

--- test/DCNTToken.test.ts
+++ test/DCNTToken.test.ts
@@ -24,26 +24,26 @@ describe("DCNTToken", async function () {
   });
 
   describe("Token features", function () {
-    describe("Minting the correct amount of tokens", function () {
+    context("on deployment", function () {
       let totalSupply: BigNumber;
 
       beforeEach(async function () {
         totalSupply = await dcnt.totalSupply();
       });
 
-      it("Should mint the correct amount of tokens in wei (decimals)", async function () {
+      it("should mint the correct amount of tokens in wei (decimals)", async function () {
         expect(totalSupply).to.equal(mintTotal);
       });
 
-      it("Should mint the correct amount of tokens in whole numbers", async function () {
+      it("should mint the correct amount of tokens in whole numbers", async function () {
         expect(parseInt(ethers.utils.formatEther(totalSupply))).to.eq(
           mintWhole
         );
       });
     });
 
-    describe("Burning tokens", function () {
-      it("Should allow users to burn their tokens", async function () {
+    describe("burn", function () {
+      it("should allow users to burn their tokens", async function () {
         const totalSupply = await dcnt.totalSupply();
         const burnWhole = 500_000_000;
         const burnTotal = ethers.utils.parseEther(burnWhole.toString());
@@ -56,11 +56,12 @@ describe("DCNTToken", async function () {
       });
     });
 
-    describe("Minting more tokens", function () {
+    describe("mint", function () {
       let originalTotalSupply: BigNumber,
         minimumMintInterval: number,
         mintCapBPs: number,
-        nextMint: BigNumber;
+        nextMint: BigNumber,
+        maxToMint: BigNumber;
 
       beforeEach(async function () {
         [originalTotalSupply, minimumMintInterval, mintCapBPs, nextMint] =
@@ -71,81 +72,70 @@ describe("DCNTToken", async function () {
             dcnt.nextMint(),
           ]);
 
+        maxToMint = originalTotalSupply.mul(mintCapBPs).div(10000);
+
         await time.increaseTo(nextMint.toNumber());
       });
 
-      describe("Depending on the caller address", function () {
-        let oneWei: number;
-
-        beforeEach(function () {
-          oneWei = 1;
-        });
-
-        it("Should allow owner to mint 1 wei", async function () {
-          await dcnt.mint(owner.address, oneWei);
+      context("when caller address is the owner", function () {
+        it("mints 1 wei", async function() {
+          await dcnt.mint(owner.address, 1);
           expect(await dcnt.totalSupply()).to.eq(
-            originalTotalSupply.add(oneWei)
+            originalTotalSupply.add(1)
           );
         });
+      });
 
-        it("Should not allow non-owner to mint 1 wei", async function () {
+
+      context("when caller address is non-owner", function() {
+        it("reverts with correct error message", async function () {
           await expect(
-            dcnt.connect(nonOwner).mint(owner.address, oneWei)
+            dcnt.connect(nonOwner).mint(owner.address, 1)
           ).to.be.revertedWith("Ownable: caller is not the owner");
         });
       });
 
-      describe("Depending on the amount", function () {
-        let maxToMint: BigNumber;
-
-        beforeEach(async function () {
-          // calculate the max amount of tokens that can be minted
-          maxToMint = originalTotalSupply.mul(mintCapBPs).div(10000);
-        });
-
-        describe("Max mint amount", function () {
-          it("Should allow the owner to mint more tokens", async function () {
-            await dcnt.mint(owner.address, maxToMint);
-            expect(await dcnt.totalSupply()).to.eq(
-              originalTotalSupply.add(maxToMint)
-            );
-          });
-        });
-
-        describe("Over max mint amount", function () {
-          let overMaxToMint: BigNumber;
-
-          beforeEach(function () {
-            overMaxToMint = maxToMint.add(1);
-          });
-
-          it("Should not allow the owner to mint more tokens", async function () {
-            await expect(
-              dcnt.mint(owner.address, overMaxToMint)
-            ).to.be.revertedWith("MintExceedsMaximum()");
-          });
+      context("when minting the max tokens allowed", function () {
+        it("should allow the owner to mint more tokens", async function() {
+          await dcnt.mint(owner.address, maxToMint);
+          expect(await dcnt.totalSupply()).to.eq(
+            originalTotalSupply.add(maxToMint)
+          );
         });
       });
 
-      describe("Depending on the time", function () {
+      context("when minting over the max mint amount", function () {
+        it("should not allow the owner to mint more tokens", async function () {
+          await expect(
+            dcnt.mint(owner.address, maxToMint.add(1))
+          ).to.be.revertedWith("MintExceedsMaximum()");
+        });
+      });
+
+      context("after new mint", function () {
         beforeEach(async function () {
           // dummy mint to set the timeout interval
           await dcnt.mint(owner.address, 0);
         });
 
-        it("Should not allow the owner to mint after having just minted", async function () {
+        it("should not allow the owner to mint after having just minted", async function () {
           await expect(dcnt.mint(owner.address, 1)).to.be.revertedWith(
             "MintTooSoon()"
           );
         });
 
-        it("Should allow the owner to mint after the minimum mint interval", async function () {
-          await time.increase(minimumMintInterval);
-          const toMint = 1;
-          await dcnt.mint(owner.address, toMint);
-          expect(await dcnt.totalSupply()).to.eq(
-            originalTotalSupply.add(toMint)
-          );
+        context("after minimum interval has passed since last mint", function() {
+          beforeEach(async function() {
+            await time.increase(minimumMintInterval);
+          });
+
+          it("should allow the owner to mint after the minimum mint interval", async function() {
+            const toMint = 1;
+            await dcnt.mint(owner.address, toMint);
+            expect(await dcnt.totalSupply()).to.eq(
+              originalTotalSupply.add(toMint)
+            );
+          });
         });
       });
     });
