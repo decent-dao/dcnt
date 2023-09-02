1228ac1940638aca76a2319015fd1f153a518919
Adam Gall <galladamj@gmail.com>
2023-09-01 21:04:11 -0400

    Add another implementation of IDCNTMintAuthorization, which never allows any minting

---
+++ contracts/mint/NoMint.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.19;
+
+import { IDCNTMintAuthorization, IERC20 } from "./IDCNTMintAuthorization.sol";
+
+/// @notice an implementation of IDCNTMintAuthorization that allows all mint requests
+contract NoMint is IDCNTMintAuthorization {
+    
+    /// @notice simply returns true, indicating that the mint request is authorized
+    function authorizeMint(address, uint256) external pure returns (bool) {
+        return false;
+    }
+}
